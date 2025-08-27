60611b2b2451fe3c7f5cb683a37e682a15bb0b50
alfred0zhu <alfred@youland.com>
2025-08-27 14:04:26 +0800

    refactor: remove code

--- src/components/molecules/ProspectDetail/ProspectDetailContent.tsx
+++ src/components/molecules/ProspectDetail/ProspectDetailContent.tsx
@@ -33,9 +33,8 @@ export const ProspectDetailContent: FC<ProspectDetailTableProps> = ({
     updateColumnName,
     updateColumnPin,
     updateColumnVisible,
-    deleteColumn,
-    setActiveColumnId,
     openDialog,
+    setActiveColumnId,
   } = useProspectTableStore((store) => store);
   const { setOpen, setSchemaJson, setPrompt, setGenerateDescription } =
     useWebResearchStore((store) => store);
