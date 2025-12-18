68f1bf63c89a073b85ecfc08fd84ef7e4293d6bf
alfred0zhu <alfred@youland.com>
2025-12-18 09:18:26 +0800

    refactor: button disabled

--- src/components/molecules/EnrichmentDetail/Dialog/WebResearch/DialogWebResearch.tsx
+++ src/components/molecules/EnrichmentDetail/Dialog/WebResearch/DialogWebResearch.tsx
@@ -372,6 +372,7 @@ export const DialogWebResearch: FC<DialogWebResearchProps> = ({
       {/* footer */}
       <DialogFooter
         coinsPerRow={COINS_PER_ROW}
+        disabled={generateIsThinking}
         loading={
           state.loading || saveAndRunState.loading || updateState.loading
         }
